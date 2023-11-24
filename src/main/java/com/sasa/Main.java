6159eb08140afb33da40dfa83177aaa5ee183adb
xiaosa <1811984305@qq.com>
2023-11-24 14:22:38 +0800

    主分支为了有冲突的一次修改

--- src/main/java/com/sasa/Main.java
+++ src/main/java/com/sasa/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("王婧琪");
         System.out.println("新的 修改的内容用于测试");
+        System.out.println("主分支这里也信行了修改");
+        System.out.println("这句主分支的语句我们不保存");
         System.out.println("新的 修改的内容用于测试");
         System.out.println("新的 修改的内容用于测试");
         System.out.println("我们的在和热分支上修改庶数据");
