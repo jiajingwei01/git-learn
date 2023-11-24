cad92deab9c5d238462dd8d7d98c248ad48cfbe6
xiaosa <1811984305@qq.com>
2023-11-24 14:24:59 +0800

    主分支的冲突提交

--- src/main/java/com/sasa/Main.java
+++ src/main/java/com/sasa/Main.java
@@ -2,15 +2,6 @@ package com.sasa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("王婧琪");
-        System.out.println("为了有冲突热修改了这个文件");
-        System.out.println("新的 修改的内容用于测试");
-        System.out.println("主分支这里也信行了修改");
-        System.out.println("这句主分支的语句我们不保存");
-        System.out.println("新的 修改的内容用于测试");
-        System.out.println("新的 修改的内容用于测试");
-        System.out.println("我们的在和热分支上修改庶数据");
-        System.out.println("热分支修改两条数据~~~");
-
+        System.out.println("主分支上模拟冲突");
     }
 }
