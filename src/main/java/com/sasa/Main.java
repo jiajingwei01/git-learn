3726ca02b0b2358189e0ade3b34f840522a68ee8
jiajingwei01 <136400271+jiajingwei01@users.noreply.github.com>
2023-11-24 14:57:07 +0800

    浏览器中的修改

--- src/main/java/com/sasa/Main.java
+++ src/main/java/com/sasa/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("主分支上模拟冲突");
         System.out.println("我们现在就只有一句话");
         System.out.println("我们再次在master分支修改数据");
+        // 我们在浏览器上添数据来验证idea的pull 操作
     }
 }
