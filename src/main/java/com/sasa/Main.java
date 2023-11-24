17cff76bde334a774c27cfe6d59cfdfec3de28e5
xiaosa <1811984305@qq.com>
2023-11-24 14:24:17 +0800

    冲突第二次练习

--- src/main/java/com/sasa/Main.java
+++ src/main/java/com/sasa/Main.java
@@ -2,13 +2,6 @@ package com.sasa;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("王婧琪");
-        System.out.println("为了有冲突热修改了这个文件");
-        System.out.println("新的 修改的内容用于测试");
-        System.out.println("新的 修改的内容用于测试");
-        System.out.println("新的 修改的内容用于测试");
-        System.out.println("我们的在和热分支上修改庶数据");
-        System.out.println("热分支修改两条数据~~~");
-
+        System.out.println("我们现在就只有一句话");
     }
 }
