05bc74d63b79ea329b341687c8e2e8443ba22195
xiaosa <1811984305@qq.com>
2023-11-24 14:21:18 +0800

    热分支第二次修改

--- src/main/java/com/sasa/Main.java
+++ src/main/java/com/sasa/Main.java
@@ -3,6 +3,7 @@ package com.sasa;
 public class Main {
     public static void main(String[] args) {
         System.out.println("王婧琪");
+        System.out.println("为了有冲突热修改了这个文件");
         System.out.println("新的 修改的内容用于测试");
         System.out.println("新的 修改的内容用于测试");
         System.out.println("新的 修改的内容用于测试");
