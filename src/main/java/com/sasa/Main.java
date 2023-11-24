06d10ec2d2cf8fa809d839e11b1f751c5514e03c
xiaosa <1811984305@qq.com>
2023-11-24 14:41:39 +0800

    主分支的冲突提交

--- src/main/java/com/sasa/Main.java
+++ src/main/java/com/sasa/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("主分支上模拟冲突");
         System.out.println("我们现在就只有一句话");
+        System.out.println("我们再次在master分支修改数据");
     }
 }
