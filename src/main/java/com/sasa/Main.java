f4377621a17820344e9ff161d66afe73203c53ab
xiaosa <1811984305@qq.com>
2023-11-24 14:13:02 +0800

    热分支提交一次修改操作

--- src/main/java/com/sasa/Main.java
+++ src/main/java/com/sasa/Main.java
@@ -6,6 +6,8 @@ public class Main {
         System.out.println("新的 修改的内容用于测试");
         System.out.println("新的 修改的内容用于测试");
         System.out.println("新的 修改的内容用于测试");
+        System.out.println("我们的在和热分支上修改庶数据");
+        System.out.println("热分支修改两条数据~~~");
 
     }
 }
