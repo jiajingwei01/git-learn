fe8a217b04ecc65898f7de0342ef5f60614e8318
xiaosa <1811984305@qq.com>
2023-11-24 11:06:38 +0800

    第三次添加，我们一共写了4条输出语句

--- src/main/java/com/sasa/Main.java
+++ src/main/java/com/sasa/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("王婧琪");
         System.out.println("新的 修改的内容用于测试");
+        System.out.println("新的 修改的内容用于测试");
+        System.out.println("新的 修改的内容用于测试");
 
     }
 }
